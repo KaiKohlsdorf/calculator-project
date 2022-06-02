517c2878e067b811b7a6033d2a09658c41880f41
Donovan Weber <donovanweber03@gmail.com>
2022-06-02 12:43:55 -0400

    Add ui logic and form
    Co-authored-by: bpgoodrow <bpgoodrow@users.noreply.github.com>
    Co-authored-by: KaiKohlsdorf <KaiKohlsdorf@users.noreply.github.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,39 +1,25 @@
-function add(number1, number2){
+function add(number1, number2) {
   return number1 + number2;
-  }
-  
-  let number1 = parseInt(prompt("Enter a number:"));
-  
-  let number2 = parseInt(prompt("Enter another number:"));
-  
-  alert(add(number1, number2));
-
-function sub(number3, number4){
-  return number3 - number4;
 }
 
-let number3 = parseInt(prompt("Enter a number you want to subtract"));
-
-let number4 = parseInt(prompt("Enter a number to subtract from original"))
-
-alert(sub(number3, number4));
-
-function times(number5, number6) {
-  return number5 * number6;
+function subtract(number1, number2) {
+  return number1 - number2;
 }
 
-let number5 = parseInt(prompt("Enter a number you want to multiply"));
-
-let number6 = parseInt(prompt("Enter a number to multiply from original"))
-
-alert(times(number5, number6));
-
-function divide(number7, number8) {
-  return number7 / number8;
+function multiply(number1, number2) {
+  return number1 * number2;
 }
 
-let number7 = parseInt(prompt("Enter a number you want to divide"));
+function divide(number1, number2) {
+  return number1 / number2;
+}
 
-let number8 = parseInt(prompt("Enter a number to divide from original"))
-
-alert(divide(number7, number8));
+$(document).ready(function() {
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+    const number1 = parseInt($("#add1").val());
+    const number2 = parseInt($("#add2").val());
+    const result = add(number1, number2);
+    $("#output").text(result);
+  });
+});
